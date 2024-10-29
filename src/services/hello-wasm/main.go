c804f5fb748959ff201d3ea25bbe99ac05c24e9d
Rajiv Singh <rajiv.singh@maersk.com>
2024-10-29 16:29:44 +0530

    add
    Signed-off-by: Rajiv Singh <rajiv.singh@maersk.com>

--- src/services/hello-wasm/main.go
+++ src/services/hello-wasm/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/rajiv-maersk/service/hello-wasm/helpers"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -11,6 +12,8 @@ func main() {
 	sum := helpers.Add(a, b)
 	difference := helpers.Sub(a, b)
 
+	logger := zap.NewExample()
+	logger.Info("Sum", zap.Int("sum", sum))
 	fmt.Printf("Sum: %d\n", sum)
 	fmt.Printf("Difference: %d\n", difference)
 }
